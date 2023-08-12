fcc86ee6533982a6792f43c40461d25f31da8c45
Gyovani <gyovani.santos@dcomp.ufs.br>
2023-08-12 00:13:19 -0300

    update persons

--- test.rb
+++ test.rb
@@ -1,1 +1,44 @@
-puts "oi,"
+class RichPerson
+    def money
+        1000000
+    end
+end 
+
+class PoorPerson
+    def money
+        1000
+    end
+end 
+
+class Person < PoorPerson
+    attr_accessor :name, :age
+ 
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def name
+        @name
+    end
+
+    def age
+        @age
+    end
+
+    def introduce 
+        "Olá, Meu nome é #{@name} e eu tenho #{@age} anos de idade coroi, hehehe!"
+    end
+
+    
+
+    def adult?
+        @age>=18
+    end
+
+    def adult!
+        @age = 20
+    end
+
+end
